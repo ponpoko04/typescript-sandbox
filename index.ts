66f768ea60ce4501d1e54b698a83909dcb18921b
ponpoko04 <taum0715@gmail.com>
2019-11-19 23:41:11 +0900

    tuple

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
 let message : string = "ほげ！";
 console.log(message);
+
+// Declare a tuple type
+let x: [string, number];
+// Initialize it
+x = ["hello", 10]; // OK
+// Initialize it incorrectly
+// x = [10, "hello"]; // Error
+
+console.log(x[0].substring(1)); // OK
+// console.log(x[1].substring(1)); // Error, 'number' does not have 'substring'
+
+// x[3] = "world"; // Error, Property '3' does not exist on type '[string, number]'.
+
+// console.log(x[5].toString()); // Error, Property '5' does not exist on type '[string, number]'.
