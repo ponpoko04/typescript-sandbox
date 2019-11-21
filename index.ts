94fedcb3993f4df23d6cf9c183fbc54f0285b554
ponpoko04 <taum0715@gmail.com>
2019-11-21 22:30:55 +0900

    tuple destructuring

--- index.ts
+++ index.ts
@@ -14,3 +14,29 @@ console.log(x[0].substring(1)); // OK
 // x[3] = "world"; // Error, Property '3' does not exist on type '[string, number]'.
 
 // console.log(x[5].toString()); // Error, Property '5' does not exist on type '[string, number]'.
+
+function f([first, second]: [number, number]) {
+    console.log(first);
+    console.log(second);
+}
+f([1, 2]);
+// f([1, 2, 3]);   // Argument of type '[number, number, number]' is not assignable to parameter of type '[number, number]'.
+
+let tuple: [number, string, boolean] = [7, "hello", true];
+// let [a, b, c] = tuple; // a: number, b: string, c: boolean
+// let [a, b, c, d] = tuple; // Error, no element at index 3
+
+// let [a, ...bc] = tuple; // bc: [string, boolean]
+// console.log(`a:${a}`);
+// console.log(`bc:${bc}`);
+
+// let [a, b, c, ...d] = tuple; // d: [], the empty tuple
+// console.log(`a:${a}`);
+// console.log(`b:${b}`);
+// console.log(`c:${c}`);
+// console.log(`d:${d}`);
+
+let [a] = tuple; // a: number
+let [, b] = tuple; // b: string
+console.log(`a:${a}`);
+console.log(`b:${b}`);
